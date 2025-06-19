f0f864ef08e01a6a231cf312f150dab4aaa41655
Toufiq <toufiquer1998@gmail.com>
2025-06-20 01:49:03 +0600

    first commit

---
+++ src/Router/Router.jsx
@@ -0,0 +1,25 @@
+
+import { createBrowserRouter } from 'react-router';
+import RootLayout from '../layouts/RootLayout';
+import Home from '../Pages/Home/Home';
+import Register from '../Pages/Register/Register';
+
+const Router = createBrowserRouter([
+    {
+        path: "/",
+        Component: RootLayout,
+        children: [
+            {
+                index: true,
+                Component: Home
+            },
+            {
+                path: '/register',
+                Component: Register
+            }
+        ],
+    },
+]);
+
+
+export default Router;
---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import Footer from '../Pages/Shared/Footer';
+import Navbar from '../Pages/Shared/Navbar';
+
+const RootLayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+            
+        </div>
+    );
+};
+
+export default RootLayout;
